58220da3489875a665bcd7531589191352b50a7b
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-10-13 21:01:36 +0900

    Create arr1.java

---
+++ programmers/arr1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+      
+        int[] copyArr = arr.clone();
+        Arrays.sort(copyArr);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < copyArr.length; i++){
+            if(arr[i] != copyArr[0])
+                list.add(arr[i]);
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
