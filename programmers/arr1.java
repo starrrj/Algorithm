0b060a61ce2cc379dcd18bd6c4f8db9a9259eb89
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-10-13 21:05:00 +0900

    Update arr1.java

--- programmers/arr1.java
+++ programmers/arr1.java
@@ -2,27 +2,37 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 
-class Solution {
-    public int[] solution(int[] arr) {
-        if(arr.length == 1) {
-            arr[0] = -1;
-            return arr;
-        }
-      
-        int[] copyArr = arr.clone();
-        Arrays.sort(copyArr);
+// 제일 작은 수 제거
+public class solution {
+
+	public int[] solution(int[] arr) {
         
-        ArrayList<Integer> list = new ArrayList<>();
+		// sort를 이용해서 최솟값 가져오기    
+		int[] temp = arr.clone();
+		Arrays.sort(temp);
+		int min = temp[0];
         
-        for(int i = 0; i < copyArr.length; i++){
-            if(arr[i] != copyArr[0])
-                list.add(arr[i]);
-        }
+		//ArrayList를 이용하여 최솟값과 같지 않은 수만 추가
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] != min) {
+				list.add(arr[i]);
+			} //if
+		} // for
+		
+		int[] answer;
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        return answer;
-    }
-}
+		// 경우를 나눠서 배열에 값을 저장하기		
+		if(list.size() == 0) {
+			answer = new int[1];
+			answer[0] = -1;
+		} else {
+			answer = new int[list.size()];
+			for (int i = 0; i < list.size(); i++) {
+				answer[i] = list.get(i);
+			} // for
+		} // if-else
+		
+        return answer;        
+    } // solution
+} // end class
