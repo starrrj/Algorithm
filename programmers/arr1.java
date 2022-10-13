a51de41c7c9d4d38a64b190c607e142b7603f48f
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-10-13 21:15:06 +0900

    Update arr1.java

--- programmers/arr1.java
+++ programmers/arr1.java
@@ -1,38 +1,31 @@
-import java.util.Arrays;
-import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] arr) {
+        // 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴
+        if(arr.length == 1) {
+            return new int[]{-1};
+            
+        } else {
+             
+            int[] answer = new int[arr.length - 1];
 
+            // 가장 작은 수 구하기
+            int min = arr[0];
+            for(int i = 0; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+        
+            // answer에 가장 작은 수 빼고 채우기
+            int count = 0;
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] == min) continue;
+                answer[count++] = arr[i];
+            } 
+                      
+        return answer;
+        } // if-else
 
-// 제일 작은 수 제거
-public class solution {
-
-	public int[] solution(int[] arr) {
-        
-		// sort를 이용해서 최솟값 가져오기    
-		int[] temp = arr.clone();
-		Arrays.sort(temp);
-		int min = temp[0];
-        
-		//ArrayList를 이용하여 최솟값과 같지 않은 수만 추가
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < arr.length; i++) {
-			if(arr[i] != min) {
-				list.add(arr[i]);
-			} //if
-		} // for
-		
-		int[] answer;
-        
-		// 경우를 나눠서 배열에 값을 저장하기		
-		if(list.size() == 0) {
-			answer = new int[1];
-			answer[0] = -1;
-		} else {
-			answer = new int[list.size()];
-			for (int i = 0; i < list.size(); i++) {
-				answer[i] = list.get(i);
-			} // for
-		} // if-else
-		
-        return answer;        
     } // solution
+       
 } // end class
