063289d96271072b23dfadc5091c5b7271da490d
좋은별 <96775458+starrrj@users.noreply.github.com>
2022-11-21 13:24:37 +0900

    Add files via upload

---
+++ w3resource/basicPart1/Ex040.java
@@ -0,0 +1,17 @@
+package basicpart1;
+
+
+/*
+ *  주어진 문자의 ASCII 값을 인쇄하는 Java 프로그램을 작성하십시오.
+ */
+public class Ex040 {
+
+	public static void main(String[] args){
+
+		int charNum = 'Z';
+		
+		System.out.println("ASCII 값 : " + charNum);
+		
+	} // main
+	
+} // end class
